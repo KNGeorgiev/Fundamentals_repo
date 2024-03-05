320f146721609af9fd35f7be63e4f30226b15438
Kalin Georgiev <144262020+KNGeorgiev@users.noreply.github.com>
2024-03-05 21:42:15 +0200

    Update 05_orders.py

--- 10_functions_lab/05_orders.py
+++ 10_functions_lab/05_orders.py
@@ -1,1 +1,22 @@
+product = input()
+qty = float(input())
 
+
+def price_calculator(product, qty):
+
+    if product == "coffee":
+        cost = qty * 1.50
+
+    elif product == "coke":
+        cost = qty * 1.40
+
+    elif product == "water":
+        cost = qty * 1.00
+
+    elif product == "snacks":
+        cost = qty * 2.00
+
+    return cost
+
+
+print(f"{price_calculator(product, qty):.2f}")
