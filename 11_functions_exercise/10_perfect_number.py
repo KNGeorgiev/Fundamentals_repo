ad2de08b65b0e812d228fd783e3e19e194d51d43
Kalin Georgiev <144262020+KNGeorgiev@users.noreply.github.com>
2024-03-17 18:58:30 +0200

    Update 10_perfect_number.py

--- 11_functions_exercise/10_perfect_number.py
+++ 11_functions_exercise/10_perfect_number.py
@@ -1,1 +1,16 @@
+def perfect_number(n):
+    perfect_container = 0
 
+    for i in range(1, n):
+        if n % i == 0:
+            perfect_container += i
+
+    if perfect_container == n:
+        print("We have a perfect number!")
+    else:
+        print("It's not so perfect.")
+
+
+number = int(input())
+
+perfect_number(number)
