8541fafc69f35a2c364e2c2e1bab43775edc3125
Kalin Georgiev <144262020+KNGeorgiev@users.noreply.github.com>
2024-03-17 18:58:47 +0200

    Update 11_loading_bar.py

--- 11_functions_exercise/11_loading_bar.py
+++ 11_functions_exercise/11_loading_bar.py
@@ -1,1 +1,15 @@
+def loading_bar(n):
+    symbol_multiplier = n // 10
+    empty_part = 10 - symbol_multiplier
+    bar = (symbol_multiplier * '%') + (empty_part * '.')
 
+    if empty_part == 0:
+        print('100% Complete!')
+        print(f'[{bar}]')
+    else:
+        print(f'{n}% [{bar}]')
+        print('Still loading...')
+
+
+loaded_percentage = int(input())
+loading_bar(loaded_percentage)
