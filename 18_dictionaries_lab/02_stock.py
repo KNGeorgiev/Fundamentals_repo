565390aed5086888fc2eb11fb560fb92adbe582f
Kalin Georgiev <144262020+KNGeorgiev@users.noreply.github.com>
2024-04-17 23:22:36 +0300

    Create 02_stock.py

---
+++ 18_dictionaries_lab/02_stock.py
@@ -0,0 +1,15 @@
+elements = input().split(' ')
+search = input().split(' ')
+bakery = {}
+
+for i in range(0, len(elements), 2):
+    key = elements[i]
+    value = elements[i + 1]
+    bakery[key] = int(value)
+
+for product in search:
+    if product in bakery:
+        print(f"We have {bakery[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
+        
